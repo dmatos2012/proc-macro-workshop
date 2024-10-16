d28d1d5d5930835decf1593e294afda7ba4c4ad8
David Matos <david@track32.nl>
2024-10-16 11:23:41 +0200

    cleanup and make it much understandable

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -3,15 +3,33 @@ use proc_macro2::Span;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use syn::visit_mut::VisitMut;
-use syn::{Expr, Ident, Pat, Stmt};
+use syn::{Ident, Pat};
 
-struct MatchVisitor;
+struct MatchVisitor {
+    idents: Vec<Ident>,
+}
+
+impl MatchVisitor {
+    fn new() -> Self {
+        MatchVisitor { idents: Vec::new() }
+    }
+}
 
 impl VisitMut for MatchVisitor {
     fn visit_expr_match_mut(&mut self, node: &mut syn::ExprMatch) {
         // do it only if it has a `sorted` attribute, not always
         // these can be modified later.
+        //node.attrs.clear();
+        for arm in &node.arms {
+            if let Pat::TupleStruct(tuple_struct) = arm.clone().pat {
+                let segments = tuple_struct.path.segments;
+                for segment in segments {
+                    self.idents.push(segment.clone().ident);
+                }
+            }
+        }
         node.attrs.clear();
+        syn::visit_mut::visit_expr_match_mut(self, node);
     }
 }
 
@@ -26,17 +44,14 @@ fn check_variants_sorted(idents: &Vec<Ident>) -> std::result::Result<(), syn::Er
     // If its empty, it means its ordered correctly
     match res.next() {
         None => Ok(()),
-        Some((unsorted_item, sorted_item)) => {
-            //panic!("hi");
-            Err(syn::Error::new(
-                sorted_item.span(),
-                format!(
-                    "{} should sort before {}",
-                    sorted_item.to_string(),
-                    unsorted_item.to_string()
-                ),
-            ))
-        }
+        Some((unsorted_item, sorted_item)) => Err(syn::Error::new(
+            sorted_item.span(),
+            format!(
+                "{} should sort before {}",
+                sorted_item.to_string(),
+                unsorted_item.to_string()
+            ),
+        )),
     }?;
     Ok(())
 }
@@ -62,81 +77,47 @@ fn sort_enum(input: &TokenStream) -> std::result::Result<TokenStream2, syn::Erro
     }
 }
 
-fn get_match_arm_idents(input: &TokenStream) -> std::result::Result<Vec<Ident>, syn::Error> {
-    let fn_item = syn::parse::<syn::ItemFn>(input.to_owned())?;
-    let stmts = fn_item.clone().block.stmts;
-    let match_idents_big = stmts
-        .into_iter()
-        .filter_map(|stmt| {
-            if let Stmt::Expr(expr, _) = stmt {
-                if let Expr::Match(expr_match) = expr {
-                    let arms = expr_match.arms;
-                    let idents1_big = arms
-                        .iter()
-                        .map(|arm| {
-                            if let Pat::TupleStruct(tuple_struct) = arm.clone().pat {
-                                let segments = tuple_struct.path.segments;
-                                let idents = segments
-                                    .iter()
-                                    .map(|segment| segment.clone().ident)
-                                    .collect::<Vec<Ident>>();
-                                idents
-                            } else {
-                                vec![]
-                            }
-                        })
-                        .collect::<Vec<Vec<Ident>>>();
-                    // this probably can be done inside all the filter maps code, but want to make it work first.
-                    let idents1 = idents1_big.into_iter().flatten().collect::<Vec<Ident>>();
-                    Some(idents1)
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<Vec<Ident>>>();
-
-    let match_idents = match_idents_big
-        .into_iter()
-        .flatten()
-        .collect::<Vec<Ident>>();
-    Ok(match_idents)
-}
-
-fn clear_match_atts(input: &TokenStream) -> std::result::Result<TokenStream2, syn::Error> {
+fn check_enum(
+    input: &TokenStream,
+) -> std::result::Result<(TokenStream2, Option<syn::Error>), syn::Error> {
     let mut fn_item = syn::parse::<syn::ItemFn>(input.to_owned())?;
-    MatchVisitor.visit_item_fn_mut(&mut fn_item);
-    Ok(fn_item.to_token_stream())
-}
-fn check_enum(input: &TokenStream) -> std::result::Result<TokenStream2, syn::Error> {
-    let mut fn_item = syn::parse::<syn::ItemFn>(input.to_owned())?;
-    MatchVisitor.visit_item_fn_mut(&mut fn_item);
-    let arm_idents = get_match_arm_idents(&input)?;
-    check_variants_sorted(&arm_idents)?;
-    Ok(fn_item.to_token_stream())
+    let mut match_visitor = MatchVisitor::new();
+    match_visitor.visit_item_fn_mut(&mut fn_item);
+    let modified_fn_item = fn_item.to_token_stream();
+    //let arm_idents = get_match_arm_idents(&input)?;
+    // I need the modified versions in both so thats why we do it like this
+    match check_variants_sorted(&match_visitor.idents) {
+        Ok(_) => Ok((modified_fn_item, None)),
+        Err(e) => Ok((modified_fn_item, Some(e))),
+    }
 }
 
 #[proc_macro_attribute]
 pub fn check(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    if let Ok(fn_item) = clear_match_atts(&input) {
-        let res = check_enum(&input);
-        match res {
-            Ok(input_token) => input_token.into(),
-            Err(e) => {
+    let res = check_enum(&input);
+    match res {
+        Ok((modified_fn_token, maybe_error)) => match maybe_error {
+            None => modified_fn_token.into(),
+            Some(e) => {
                 let err = e.to_compile_error();
-                let original_item: proc_macro2::TokenStream = fn_item.into();
                 quote! {
-                    #original_item
+                    #modified_fn_token
                     #err
                 }
                 .into()
             }
+        },
+        // this one handles syn errors, which is super unlikely
+        Err(e) => {
+            let err = e.to_compile_error();
+            let input_stream: proc_macro2::TokenStream = input.into();
+            quote! {
+                #input_stream
+                #err
+            }
+            .into()
         }
-    } else {
-        input.into()
     }
 }
 
